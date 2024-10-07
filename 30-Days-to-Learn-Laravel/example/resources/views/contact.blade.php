78e8be95d2df151a32f2e7e356fa415c71a63179
naol86 <naol.kasinet@a2sv.org>
2024-10-07 13:28:01 +0300

    day 4 slots

--- 30-Days-to-Learn-Laravel/example/resources/views/contact.blade.php
+++ 30-Days-to-Learn-Laravel/example/resources/views/contact.blade.php
@@ -1,4 +1,7 @@
 <x-layout>
+  <x-slot:heading>
+    Contact Page
+  </x-slot:heading>
   <h1>Contact Us</h1>
   <p>This is the content of the Contact page.</p>
 </x-layout>
--- 30-Days-to-Learn-Laravel/example/resources/views/welcome.blade.php
+++ 30-Days-to-Learn-Laravel/example/resources/views/welcome.blade.php
@@ -1,4 +1,7 @@
 <x-layout>
+  <x-slot:heading>
+    Home Page
+  </x-slot:heading>
   <h1>Welcome to Laravel</h1>
   <p>This is the content of the welcome page.</p>
 </x-layout>
