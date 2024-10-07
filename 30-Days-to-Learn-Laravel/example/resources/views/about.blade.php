f336d33053e179c26d530bfd2e1e0db19fa5fd9e
naol86 <naol.kasinet@a2sv.org>
2024-10-07 12:52:13 +0300

    day 3 blade and components

--- 30-Days-to-Learn-Laravel/example/resources/views/about.blade.php
+++ 30-Days-to-Learn-Laravel/example/resources/views/about.blade.php
@@ -1,21 +1,4 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-
-  <title>Laravel</title>
-
-  <!-- Fonts -->
-  <link rel="preconnect" href="https://fonts.bunny.net">
-  <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
-  <!-- Styles -->
-</head>
-
-<body class="font-sans antialiased dark:bg-black dark:text-white/50">
-  <h1>About Page</h1>
-</body>
-
-</html>
+<x-layout>
+  <h1>About Us</h1>
+  <p>This is the content of the about page.</p>
+</x-layout>
---
+++ 30-Days-to-Learn-Laravel/example/resources/views/contact.blade.php
@@ -0,0 +1,4 @@
+<x-layout>
+  <h1>Contact Us</h1>
+  <p>This is the content of the Contact page.</p>
+</x-layout>
