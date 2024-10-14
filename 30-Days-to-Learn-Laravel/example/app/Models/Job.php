0e58f0f46c476747b0a103f3756d93c9f3480f6a
naol86 <naol.kasinet@a2sv.org>
2024-10-14 11:44:26 +0300

    day 9/30

--- 30-Days-to-Learn-Laravel/example/app/Models/Job.php
+++ 30-Days-to-Learn-Laravel/example/app/Models/Job.php
@@ -2,51 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
-class Job
+class Job extends Model
 {
-    public static function all():array {
-        return [
-            [
-                'id'=> 1,
-                'title' => 'Director',
-                'salary' => 1200,
-                
-            ]
-            ,
-            [
-                'id'=> 2,
-                'title' => 'Manager',
-                'salary' => 1000,
-            ],
-            [
-                'id'=> 3,
-                'title' => 'Engineer',
-                'salary' => 900,
-            ],
-            [
-                'id'=> 4,
-                'title' => 'Technician',
-                'salary' => 800,
-            ],
-            [
-                'id'=> 5,
-                'title' => 'Clerk',
-                'salary' => 700,
-            ],
-            [
-                'id'=> 6,
-                'title' => 'Intern',
-                'salary' => 500,
-            ]
-            ];
-    }
-    public static function find(int $id): array {
-        $job = Arr::first( self::all(), fn($job) => $job['id'] == $id );
-        if (! $job) {
-            abort(404);
-        }
-        return $job;
-    }
+    protected $table = "job_listings";
+    protected $fillable = [
+        'title',
+        'salary'
+    ];
 }
